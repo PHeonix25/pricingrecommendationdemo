21aa086be35bb3d9096c25078facf530ad82940c
Pat Hermens <p.hermens@coolblue.nl>
2016-06-29 14:59:01 +0200

    Add the distinct product count to the output

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Collections.Generic;
 using Newtonsoft.Json;
@@ -17,7 +18,8 @@ namespace ConsoleApplication
             var response = client.GetAsync("").Result;
             var responseBody = response.Content.ReadAsStringAsync().Result;
             var results = JsonConvert.DeserializeObject<List<PricingRecommendation>>(responseBody);
-            Console.WriteLine($"There are currently {results.Count} unactioned pricingrecommendations");
+            Console.WriteLine($"There are currently {results.Count} unactioned pricing recommendations " +
+                              $"for {results.Select(p => p.ProductId).Distinct().Count()} distinct products");
         }
     }
 }
